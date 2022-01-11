0421fe8d7d3ae40f3bef0b4c647e703a7fa3ae40
parvinaliyevv <pervin.eliyev.00@Bk.ru>
2022-01-11 22:19:45 +0400

    modified

---
+++ homework/MyRandom.cs
@@ -0,0 +1,24 @@
+﻿namespace homework
+{
+    static class MyRandom
+    {
+
+        public static System.Random RandomProp { get; set; } = new System.Random();
+
+        public static string RandomString(int length)
+        {
+            var data = new System.Text.StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                data.Append(RandomProp.Next() % 10);
+            }
+
+            return data.ToString();
+        }
+
+        public static int RandomInteger() => RandomProp.Next();
+
+        public static double RandomDouble() => RandomProp.NextDouble();
+    }
+}
